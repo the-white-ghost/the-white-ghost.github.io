fa49a54dc0a7c5273b36cd969e3e386810ed7e6b
Ganesh <vganesh1223@gmail.com>
2021-03-03 22:03:25 +0530

    'Education_updated'

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3884af09b502ed84855234f397bcdbb.js"
+  "/precache-manifest.8c68ef26bfbe9f687c97339a25abec18.js"
 );
 
 self.addEventListener('message', (event) => {
